610eb85e51a82357b8d08c67dfcf4fc883bdd6e1
Gaston Apioli <gast.ap@hotmail.com>
2023-06-23 16:19:14 -0300

    update 23/06

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,28 @@
-const { Router } = require('express');
-const { check } = require('express-validator');
+const { Router } = require("express");
+const { check } = require("express-validator");
 
+const { validarCampos } = require("../middlewares/validar-campos");
 
-const { validarCampos } = require('../middlewares/validar-campos');
-
-
-const { login } = require('../controllers/auth');
-
+const { login, googleSignIn } = require("../controllers/auth");
 
 const router = Router();
 
-router.post('/login',[
-    check('correo', 'El correo es obligatorio').isEmail(),
-    check('password', 'La contraseña es obligatoria').not().isEmpty(),
-    validarCampos
-],login );
-
-
+router.post(
+  "/login",
+  [
+    check("correo", "El correo es obligatorio").isEmail(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    validarCampos,
+  ],
+  login
+);
+router.post(
+  "/google",
+  [
+    check("id_token", "Token de google es necesario").not().isEmpty(),
+    validarCampos,
+  ],
+  googleSignIn
+);
 
 module.exports = router;
